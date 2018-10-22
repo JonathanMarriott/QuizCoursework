df8a8aacf086221cc3cc3880d5cc7917f3c74c24
JonathanMarriott <jmarriott.75691@farnborough.ac.uk>
2018-10-22 22:40:52 +0100

    User model and service classes

---
+++ src/server/models/User.java
@@ -0,0 +1,98 @@
+package server.models;
+
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+
+
+
+
+
+    // Get IntelliJ to auto-generate a constructor, getter and setters here:
+
+    public User(int id, String firstName, String lastName, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    public static ArrayList<User> users = new ArrayList<>();
+
+    public static int nextId() {
+        int id = 0;
+        for (User u: users) {
+            if (u.getId() > id) {
+                id = u.getId();
+            }
+        }
+        return id + 1;
+    }
+
+    @SuppressWarnings("unchecked")
+    public JSONObject toJSON() {
+        JSONObject j = new JSONObject();
+        j.put("id", getId());
+        j.put("firstName", getFirstName());
+        j.put("lastName", getLastName());
+        j.put("email", getEmail());
+        j.put("password", getPassword());
+
+
+
+
+
+        return j;
+    }
+}
