055c98f762e0da6ab04fd83c48312558bfb3b41e
JonathanMarriott <jmarriott.75691@farnborough.ac.uk>
2019-01-24 00:12:35 +0000

    Starting the create quiz page

--- resources/client/js/signup.js
+++ resources/client/js/signup.js
@@ -25,23 +25,25 @@ function signUpForm() {
     const signUpForm = $('#signUpForm'); // Finds the form on the page via its ID
     signUpForm.submit(event => {  // Runs the code enclosed when the form submits
         event.preventDefault(); // Stops the forms default behaviour
-        if ($("#inputPassword").val()===$("#inputPassword2").val()){  //Checks the 2 passwords match client side to reduce server load
-            $.ajax({  // Forms the AJAX request
-                url: '/user/add',  // sends to the addUser method in user controller
-                type: 'POST', // Post request as updating DB
-                data: signUpForm.serialize(),  // serializes the form content to a text string
-                success: response => {  //Runs once the server responds
-                    if (response.startsWith('Error:')) { //checks for errors
-                        alert(response); // Alerts the user to the error
-                    } else { // otherwise the account creation was successful
-                        Cookies.set("sessionToken",response); // Sets the clients sessionToken cookie to the value from the server
-                        window.location.href = "/client/profile.html"; // Redirects the user to the profile page
-                    }
-                }
-            });}
-        else{
+        if (!($("#inputPassword").val()===$("#inputPassword2").val())) {//Checks the 2 passwords match client side to reduce server load
             alert("Passwords entered do not match"); // User alerted if the passwords do not match
         }
-
-    });
+        else if ($("#inputFirst").val().length>20 || $("#inputLast").val().length   >20) { // Checks the names are not over 20 characters
+            alert("Names are too long");
+        }
+        else{
+                $.ajax({  // Forms the AJAX request
+                    url: '/user/add',  // sends to the addUser method in user controller
+                    type: 'POST', // Post request as updating DB
+                    data: signUpForm.serialize(),  // serializes the form content to a text string
+                    success: response => {  //Runs once the server responds
+                        if (response.startsWith('Error:')) { //checks for errors
+                            alert(response); // Alerts the user to the error
+                        } else { // otherwise the account creation was successful
+                            Cookies.set("sessionToken",response); // Sets the clients sessionToken cookie to the value from the server
+                            window.location.href = "/client/profile.html"; // Redirects the user to the profile page
+                        }
+                    }
+                });}
+        });
 }
